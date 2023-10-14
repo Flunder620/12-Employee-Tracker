5fdc42621e9184226d6d88ffb1d360c9ed534a6e
Flunder620 <jguzman12310@yahoo.com>
2023-10-14 10:45:25 -0500

    added schema

--- server.js
+++ server.js
@@ -7,6 +7,29 @@ const inquirer = require("inquirer");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const questions = [
+  {
+      type: "input",
+      name: "text",
+      message: "Enter your text: "
+  },
+  {
+      type: "input",
+      name: "tcolor",
+      message: "What color what you like your text?(Red, Green, Blue)"
+  },
+  {
+      type: "input",
+      name: "shape",
+      message: "Which shape would you like?(Circle, Triangle, or Square)"
+  },
+  {
+      type: "input",
+      name: "scolor",
+      message: "What color would you like your shape?(Red, Green, Blue)"
+  }
+];
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -31,6 +54,9 @@ db.query("SELECT * FROM students", function (err, results) {
 app.use((req, res) => {
   res.status(404).end();
 });
+
+//Inquirer prompt
+inquirer.prompt(questions)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
